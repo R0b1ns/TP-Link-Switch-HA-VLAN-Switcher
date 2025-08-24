31d33edba4c86a4ab13c6ffdb984e326aa4b92d3
R0b1ns <r0b1ns@brogit.de>
2025-08-24 23:37:00 +0200

    Added base for device

---
+++ custom_components/tp_link_vlan_switcher/entity_base.py
@@ -0,0 +1,22 @@
+from homeassistant.helpers.entity import Entity
+from .const import DOMAIN, CONF_IP, CONF_DEVICE
+
+class TPLinkSmartSwitchBaseEntity(Entity):
+    """Base entity with shared device info."""
+
+    def __init__(self, config_entry):
+        self._config_entry = config_entry
+        self._ip = config_entry.data[CONF_IP]
+        self._device_info = config_entry.data.get(CONF_DEVICE, {})
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._ip)},
+            "connections": {("mac", self._device_info.get("macStr"))} if self._device_info.get("macStr") else None,
+            "name": self._device_info.get("descriStr", f"VLAN Switch {self._ip}"),
+            "manufacturer": "TP-Link",
+            "model": self._device_info.get("hardwareStr").split(" ", 1)[0],
+            "sw_version": self._device_info.get("firmwareStr"),
+            "hw_version": self._device_info.get("hardwareStr").split(" ", 1)[1],
+        }
