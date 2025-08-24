ba44e264f2a78923b06080370843421dc8b51798
R0b1ns <r0b1ns@brogit.de>
2025-08-24 17:31:39 +0200

    Fix: Create error

--- custom_components/tp_link_vlan_switcher/__init__.py
+++ custom_components/tp_link_vlan_switcher/__init__.py
@@ -1,17 +1,23 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType
 from .const import DOMAIN
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "switch")
-    )
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up a config entry."""
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
+
+    # Weiterleitung an Switch-Plattform
+    await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_unload(entry, "switch")
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+    """Unload a config entry."""
+    success = await hass.config_entries.async_forward_entry_unloads(entry, ["switch"])
+    if success:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return success
