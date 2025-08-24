45ea3a21db57fec3dac86a9db6633981406e4490
R0b1ns <r0b1ns@brogit.de>
2025-08-25 00:13:07 +0200

    Fix to create device

--- custom_components/tp_link_vlan_switcher/button.py
+++ custom_components/tp_link_vlan_switcher/button.py
@@ -1,5 +1,8 @@
+import logging
 from homeassistant.components.button import ButtonEntity
 from .entity_base import TPLinkSmartSwitchBaseEntity
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up button entities for TP-Link VLAN Switch."""
@@ -15,11 +18,14 @@ class RebootButton(TPLinkSmartSwitchBaseEntity, ButtonEntity):
 
     _attr_name = "Reboot Switch"
 
+    def __init__(self, config_entry):
+        super().__init__(config_entry)
+        self._attr_unique_id = f"{self._ip}_reboot"
+
     async def async_press(self) -> None:
         """Send reboot command to the switch."""
         ip = self._ip
-        # Hier kommt dein HTTP-Call rein, z.B. POST /reboot.cgi
-        print(f"Reboot command sent to {ip}")
+        _LOGGER.debug("Reboot command sent to %s", ip)
         # TODO: Implementiere echten HTTP-Request
 
 
@@ -28,9 +34,12 @@ class ResetButton(TPLinkSmartSwitchBaseEntity, ButtonEntity):
 
     _attr_name = "Reset Switch"
 
+    def __init__(self, config_entry):
+        super().__init__(config_entry)
+        self._attr_unique_id = f"{self._ip}_reset"
+
     async def async_press(self) -> None:
         """Send reset command to the switch."""
         ip = self._ip
-        # Hier kommt dein HTTP-Call rein, z.B. POST /reset.cgi
-        print(f"Reset command sent to {ip}")
+        _LOGGER.debug("Reset command sent to %s", ip)
         # TODO: Implementiere echten HTTP-Request
