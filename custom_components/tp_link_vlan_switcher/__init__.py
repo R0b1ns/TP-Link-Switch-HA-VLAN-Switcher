cdef38df734a12df530e0b708d6f06e8f7a65314
R0b1ns <r0b1ns@brogit.de>
2025-08-24 20:17:48 +0200

    Update

--- custom_components/tp_link_vlan_switcher/__init__.py
+++ custom_components/tp_link_vlan_switcher/__init__.py
@@ -2,22 +2,26 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN
 
+PLATFORMS = ["switch"]
+
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the integration."""
     hass.data.setdefault(DOMAIN, {})
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up a config entry."""
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
 
-    # Weiterleitung an Switch-Plattform
-    await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
+    # Reload bei Options-Änderungen (z. B. wenn du in der OptionsFlow neue Switch-Profile anlegst)
+    async def _update_listener(hass: HomeAssistant, updated_entry: ConfigEntry):
+        await hass.config_entries.async_reload(updated_entry.entry_id)
+
+    entry.async_on_unload(entry.add_update_listener(_update_listener))
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    success = await hass.config_entries.async_forward_entry_unloads(entry, ["switch"])
-    if success:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return success
+    unload_ok = await hass.config_entries.async_forward_entry_unloads(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+    return unload_ok
