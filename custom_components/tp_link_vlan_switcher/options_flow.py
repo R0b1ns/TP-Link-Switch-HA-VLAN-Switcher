8465e0f85189d35770d8531c373a7719b4999f9a
R0b1ns <r0b1ns@brogit.de>
2025-08-26 20:10:16 +0200

    Improoved options flow

--- custom_components/tp_link_vlan_switcher/options_flow.py
+++ custom_components/tp_link_vlan_switcher/options_flow.py
@@ -1,6 +1,8 @@
+import json
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
+from homeassistant.helpers import selector
 from .const import DOMAIN
 
 
@@ -11,6 +13,7 @@ class VlanSwitchOptionsFlowHandler(config_entries.OptionsFlow):
         """Initialize options flow."""
         self.config_entry = config_entry
         self.switches = config_entry.options.get("switches", {})
+        self._edit_name = None
 
     async def async_step_init(self, user_input=None):
         """Menu for options flow."""
@@ -20,6 +23,8 @@ class VlanSwitchOptionsFlowHandler(config_entries.OptionsFlow):
                 return await self.async_step_add_switch()
             if action == "remove":
                 return await self.async_step_remove_switch()
+            if action == "edit":
+                return await self.async_step_edit_switch()
 
         schema = vol.Schema(
             {
@@ -27,6 +32,7 @@ class VlanSwitchOptionsFlowHandler(config_entries.OptionsFlow):
                     {
                         "add": "Neuen Profil-Switch hinzufügen",
                         "remove": "Vorhandenen Profil-Switch entfernen",
+                        "edit": "Vorhandenen Profil-Switch bearbeiten",
                     }
                 )
             }
@@ -35,26 +41,62 @@ class VlanSwitchOptionsFlowHandler(config_entries.OptionsFlow):
 
     async def _finish(self):
         """Create the options entry after any change."""
-        # <<<< important: this triggers update_listener + reload
         return self.async_create_entry(title="", data={"switches": self.switches})
 
+    # -------------------------------
+    # ADD
+    # -------------------------------
     async def async_step_add_switch(self, user_input=None):
         """Add a new profile switch."""
         if user_input is not None:
-            name = user_input["name"]
+            try:
+                name = user_input["name"]
+                vlans = json.loads(user_input["vlans"])
+                pvid = json.loads(user_input["pvid"])
+            except (ValueError, KeyError):
+                return self.async_show_form(
+                    step_id="add_switch",
+                    data_schema=self._get_add_schema(),
+                    errors={"base": "invalid_json"},
+                )
+
             self.switches[name] = {
-                "vlans": {},
-                "pvid": {},
+                "vlans": vlans,
+                "pvid": pvid,
             }
             return await self._finish()
 
-        schema = vol.Schema(
+        return self.async_show_form(
+            step_id="add_switch",
+            data_schema=self._get_add_schema(),
+        )
+
+    def _get_add_schema(self):
+        """Return schema for adding a switch."""
+        vlan_template = """{
+  "10": "Office",
+  "20": "IoT"
+}"""
+        pvid_template = """{
+  "1": "Default",
+  "10": "Office-Port"
+}"""
+
+        return vol.Schema(
             {
                 vol.Required("name"): str,
+                vol.Required("vlans", default=vlan_template): selector.TextSelector(
+                    selector.TextSelectorConfig(multiline=True)
+                ),
+                vol.Required("pvid", default=pvid_template): selector.TextSelector(
+                    selector.TextSelectorConfig(multiline=True)
+                ),
             }
         )
-        return self.async_show_form(step_id="add_switch", data_schema=schema)
 
+    # -------------------------------
+    # REMOVE
+    # -------------------------------
     async def async_step_remove_switch(self, user_input=None):
         """Remove an existing profile switch."""
         if user_input is not None:
@@ -72,6 +114,67 @@ class VlanSwitchOptionsFlowHandler(config_entries.OptionsFlow):
         )
         return self.async_show_form(step_id="remove_switch", data_schema=schema)
 
+    # -------------------------------
+    # EDIT
+    # -------------------------------
+    async def async_step_edit_switch(self, user_input=None):
+        """Choose which switch to edit."""
+        if user_input is not None:
+            self._edit_name = user_input["name"]
+            return await self.async_step_edit_switch_details()
+
+        if not self.switches:
+            return await self._finish()
+
+        schema = vol.Schema(
+            {
+                vol.Required("name"): vol.In(list(self.switches.keys())),
+            }
+        )
+        return self.async_show_form(step_id="edit_switch", data_schema=schema)
+
+    async def async_step_edit_switch_details(self, user_input=None):
+        """Edit details of a switch (vlans and pvid)."""
+        current = self.switches[self._edit_name]
+
+        if user_input is not None:
+            try:
+                vlans = json.loads(user_input["vlans"])
+                pvid = json.loads(user_input["pvid"])
+            except (ValueError, KeyError):
+                return self.async_show_form(
+                    step_id="edit_switch_details",
+                    data_schema=self._get_edit_schema(current),
+                    errors={"base": "invalid_json"},
+                )
+
+            self.switches[self._edit_name] = {
+                "vlans": vlans,
+                "pvid": pvid,
+            }
+            return await self._finish()
+
+        return self.async_show_form(
+            step_id="edit_switch_details",
+            data_schema=self._get_edit_schema(current),
+        )
+
+    def _get_edit_schema(self, current: dict):
+        """Return schema for editing a switch."""
+        return vol.Schema(
+            {
+                vol.Required("vlans", default=json.dumps(current.get("vlans", {}), indent=2)): selector.TextSelector(
+                    selector.TextSelectorConfig(multiline=True)
+                ),
+                vol.Required("pvid", default=json.dumps(current.get("pvid", {}), indent=2)): selector.TextSelector(
+                    selector.TextSelectorConfig(multiline=True)
+                ),
+            }
+        )
+
+    # -------------------------------
+    # ENTRYPOINT
+    # -------------------------------
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
